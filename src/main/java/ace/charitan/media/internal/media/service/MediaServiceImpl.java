c56bb9349d4b57648825ea09e7cabf1f7e8a45b7
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-08 08:46:23 +0700

    finish basic upload

--- src/main/java/ace/charitan/media/internal/media/service/MediaServiceImpl.java
+++ src/main/java/ace/charitan/media/internal/media/service/MediaServiceImpl.java
@@ -2,6 +2,7 @@ package ace.charitan.media.internal.media.service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.cloudinary.Cloudinary;
 
+import ace.charitan.media.config.MediaConstant;
 import ace.charitan.media.internal.media.dto.InternalMediaDto;
 import ace.charitan.media.internal.media.service.MediaEnum.MediaType;
 
@@ -18,7 +20,6 @@ class MediaServiceImpl implements InternalMediaService {
 
     @Autowired
     private MediaRepository mediaRepository;
-
     @Autowired
     private Cloudinary cloudinary;
 
@@ -31,7 +32,21 @@ class MediaServiceImpl implements InternalMediaService {
         List<InternalMediaDto> existedImageList = mediaRepository.findAllByMediaTypeAndProjectId(MediaType.IMAGE,
                 projectIdUuid);
 
-                if (existedImageList.size() + files.size() > )
+        if (existedImageList.size() + files.size() > MediaConstant.MAX_IMAGES) {
+            // TODO: MAx images allowed
+        }
+
+        for (MultipartFile file : files) {
+            try {
+                Map<String, Object> uploadResponse = cloudinary.uploader().upload(file.getBytes(), Map.of());
+                System.out.println(uploadResponse);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 
 }
