51a831166fffd6ca5cb0552b1e986817c69aa50d
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-09 19:52:50 +0700

    add trust

--- src/main/java/ace/charitan/media/internal/media/controller/MediaController.java
+++ src/main/java/ace/charitan/media/internal/media/controller/MediaController.java
@@ -1,23 +1,18 @@
 package ace.charitan.media.internal.media.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import ace.charitan.media.internal.media.dto.InternalMediaDto;
 import ace.charitan.media.internal.media.service.InternalMediaService;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 class MediaController {
