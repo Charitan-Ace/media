5c9814b5a807086ebba911c016c0a8dac13eee2a
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-09 10:13:11 +0700

    fds

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS media CASCADE;
+
+CREATE TABLE media (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- created_at
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- updated_at
+    media_url VARCHAR(255),
+    public_id VARCHAR(255),
+    media_type VARCHAR(50),
+    media_format VARCHAR(50),
+    resource_type VARCHAR(50),
+    project_id VARCHAR(50)
+);
