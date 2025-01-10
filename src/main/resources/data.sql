fe34875f87b4ad1e772c3589773328b572a566b9
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-10 22:09:07 +0700

    add sql gen script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,23 +10,36 @@ CREATE TABLE media (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- For images
--- INSERT INTO media
--- (
---     media_url,
---     public_id,
---     media_type,
---     media_format,
---     resource_type,
---     project_id
--- )
--- VALUES
--- (
-
-    
-
-
-
--- );
-
+INSERT INTO -- For images
+INSERT INTO media (
+        media_url,
+        public_id,
+        media_type,
+        media_format,
+        resource_type,
+        project_id
+    )
+VALUES (
+        'https://res.cloudinary.com/doorb2fhk/image/upload/v1736521183/charitan/project/totrxi5roliazxgvby8k.jpg',
+        'charitan/project/totrxi5roliazxgvby8k',
+        'IMAGE',
+        'jpg',
+        'image',
+        '9284aa44-9bea-4647-80bd-55c413590404',
+    ),
+    (
+        'https://res.cloudinary.com/doorb2fhk/image/upload/v1736521183/charitan/project/sbsnoac8rbwpuqep51w9.jpg',
+        'charitan/project/sbsnoac8rbwpuqep51w9',
+        'IMAGE',
+        'jpg',
+        'image',
+        '9284aa44-9bea-4647-80bd-55c413590404',
+    ),
+    (
+        'https://res.cloudinary.com/doorb2fhk/image/upload/v1736521181/charitan/project/qmpmiinirmv9xxmgxtxm.jpg',
+        'charitan/project/qmpmiinirmv9xxmgxtxm',
+        'IMAGE',
+        'jpg',
+        'image',
+        '9284aa44-9bea-4647-80bd-55c413590404',
+    );
